e941ee088d40a29c7b6bc6f651d6989357d37491
Davakin111n <fidvey@googlemail.com>
2015-11-06 14:47:57 +0200

    Update - JUnit & Mockito (final)

--- src/test/java/com/jean/taxi/serviceImpl/ClientServiceImplTest.java
+++ src/test/java/com/jean/taxi/serviceImpl/ClientServiceImplTest.java
@@ -2,6 +2,7 @@ package com.jean.taxi.serviceImpl;
 
 import com.jean.taxi.daoImpl.ClientDaoImpl;
 import com.jean.taxi.entity.User;
+import com.jean.taxi.filter.ClientFilter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import org.unitils.database.annotations.Transactional;
 import org.unitils.database.util.TransactionMode;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -22,6 +24,7 @@ import static org.mockito.Mockito.verify;
 public class ClientServiceImplTest extends UnitilsJUnit4 {
     ClientServiceImpl clientService = new ClientServiceImpl();
     ClientDaoImpl clientDao = mock(ClientDaoImpl.class);
+    StringBuilder stringBuilder = new StringBuilder();
     User user = new User();
 
     @TestDataSource
@@ -30,6 +33,7 @@ public class ClientServiceImplTest extends UnitilsJUnit4 {
     @Before
     public void createUser() {
         clientDao.setDataSource(dataSource);
+        clientService.setDao(clientDao);
         user.setEmail("email");
         user.setAddress("address");
         user.setPassword("password");
@@ -39,86 +43,70 @@ public class ClientServiceImplTest extends UnitilsJUnit4 {
         user.setSkype("skype");
     }
 
+    @After
+    public void cleanDataBase() {
+        try (Connection connection = dataSource.getConnection()) {
+            int firstColumn = 1;
+            stringBuilder.append("DELETE FROM `CLIENT_GRANT`");
+            PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString());
+            preparedStatement.executeUpdate();
+            connection.commit();
+            preparedStatement.close();
+            stringBuilder.setLength(0);
+            stringBuilder.append("DELETE FROM `CLIENT`");
+            preparedStatement = connection.prepareStatement(stringBuilder.toString());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void testAddNew() {
-        clientService.setDao(clientDao);
         clientService.addNew(user);
         verify(clientDao, Mockito.times(0)).addNew(user);
     }
 
     @Test
     public void testUpdate() {
-
-    }
-
-    @Test
-    public void testSuccessLogin() {
-
+        clientService.update(user);
+        verify(clientDao, Mockito.times(0)).update(user);
     }
 
     @Test
     public void testListAllModerators() {
-
-    }
-
-    @Test
-    public void testMadeModerator() {
-
-    }
-
-    @Test
-    public void testMadeSimpleUser() {
-
-    }
-
-
-    @Test
-    public void testBanUser() {
-
-    }
-
-    @Test
-    public void testDeleteBanUser() {
-
-    }
-
-    @Test
-    public void testChangePassword() {
-
+        clientService.listAllModerators();
+        verify(clientDao).listAllModerators();
     }
 
     @Test
     public void testListSimpleUsers() {
-
+        clientService.listSimpleUsers();
+        verify(clientDao).listSimpleUsers();
     }
 
     @Test
     public void testClientListByFilter() {
-
+        ClientFilter clientFilter = new ClientFilter();
+        clientFilter.setClientType("All");
+        clientFilter.setDateValue("No limits");
+        clientService.addNew(user);
+        clientService.clientListByFilter(clientFilter);
+        verify(clientDao).clientListByFilter(clientFilter);
     }
 
     @Test
     public void testBanList() {
-
+        clientService.banList();
+        verify(clientDao).banList();
     }
 
     @Test
     public void testGetByEmail() {
-
-    }
-
-    @After
-    public void cleanDataBase() {
-        try (PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement("DELETE FROM ?")) {
-            int firstColumn = 1;
-            preparedStatement.setString(firstColumn, "`client_grant`");
-            preparedStatement.addBatch();
-            preparedStatement.setString(firstColumn, "`client`");
-            preparedStatement.addBatch();
-            preparedStatement.executeBatch();
-            preparedStatement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        clientService.addNew(user);
+        clientService.getByEmail("email");
+        verify(clientDao).getByEmail("email");
     }
 }
--- src/test/java/com/jean/taxi/serviceImpl/OrderServiceImplTest.java
+++ src/test/java/com/jean/taxi/serviceImpl/OrderServiceImplTest.java
@@ -1,6 +1,115 @@
 package com.jean.taxi.serviceImpl;
 
+import com.jean.taxi.daoImpl.OrderDaoImpl;
+import com.jean.taxi.entity.Order;
+import com.jean.taxi.entity.OrderAddress;
+import com.jean.taxi.filter.OrderFilter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 import org.unitils.UnitilsJUnit4;
+import org.unitils.database.annotations.TestDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class OrderServiceImplTest extends UnitilsJUnit4 {
+    OrderServiceImpl orderService = new OrderServiceImpl();
+    OrderDaoImpl orderDao = mock(OrderDaoImpl.class);
+    StringBuilder stringBuilder = new StringBuilder();
+    Order order = new Order();
+
+    @TestDataSource
+    DataSource dataSource;
+
+    @Before
+    public void init() {
+        orderDao.setDataSource(dataSource);
+        orderService.setDao(orderDao);
+        order.setBeginAddress("beginAddress");
+        order.setHouseNumber("houseNumber");
+        order.setPorchNumber("porchNumber");
+        order.setPhone("phone");
+        order.setContactName("contactName");
+        order.setCarOption("option");
+        order.setNote("note");
+        List list = order.getAddressList();
+        OrderAddress orderAddress = new OrderAddress();
+        orderAddress.setDestinationAddress("destAddress");
+        orderAddress.setDestinationHouseNumber("destHouseNumber");
+        orderAddress.setDestinationPorchNumber("destPorchNumber");
+        list.add(orderAddress);
+        order.setAddressList(list);
+    }
+
+    @After
+    public void cleanDatabase() {
+        try (Connection connection = dataSource.getConnection()) {
+            int firstColumn = 1;
+            stringBuilder.append("DELETE FROM `ORDER_ADDRESS`");
+            PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString());
+            preparedStatement.executeUpdate();
+            connection.commit();
+            preparedStatement.close();
+            stringBuilder.setLength(0);
+            stringBuilder.append("DELETE FROM `ORDER`");
+            preparedStatement = connection.prepareStatement(stringBuilder.toString());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddNew() {
+        orderService.addNew(order);
+        verify(orderDao, Mockito.times(0)).addNew(order);
+    }
+
+    @Test
+    public void testUpdate() {
+        orderService.update(order);
+        verify(orderDao, Mockito.times(0)).update(order);
+    }
+
+    @Test
+    public void testDeleteOrder() {
+        orderService.addNew(order);
+        orderService.deleteOrder(order.getId());
+        verify(orderDao, Mockito.times(0)).deleteOrder(order.getId());
+    }
+
+    @Test
+    public void testActiveOrderList() {
+        orderService.activeOrderList();
+        verify(orderDao).activeOrderList();
+    }
+
+    @Test
+    public void testNotActiveOrderList() {
+        orderService.notActiveOrderList();
+        verify(orderDao).notActiveOrderList();
+    }
+
+    @Test
+    public void testAccomplishedOrderList() {
+        orderService.accomplishedOrderList();
+        verify(orderDao).accomplishedOrderList();
+    }
+
+    @Test
+    public void testOrderListByFilter() {
+        OrderFilter orderFilter = new OrderFilter("All", "No limits");
+        orderService.orderListByFilter(orderFilter);
+        verify(orderDao).orderListByFilter(orderFilter);
+    }
 }
